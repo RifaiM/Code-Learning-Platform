66e3f27ff3bebe7292363ce7fc9424497eb95f45
RifaiM <43382148+RifaiM@users.noreply.github.com>
2025-08-23 02:12:34 +0700

    Update website_concept.js

--- 1. partA/website_concept.js
+++ 1. partA/website_concept.js
@@ -56,7 +56,7 @@ document.getElementById('markReadBtn').addEventListener('click', function() {
 
     // Return to overview with delay for animation
     setTimeout(() => {
-    window.location.href = 'https://rifaim.github.io/Code-Learning-Platform';
+    window.location.href = '/index.html#before-code';
     }, 1500);
 });
 
@@ -89,11 +89,4 @@ style.textContent = `
     50% { transform: translateY(-5px); }
     }
 `;
-
 document.head.appendChild(style);
-
-
-
-
-
-
